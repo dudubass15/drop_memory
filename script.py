15065319a32a7efff834bbe3ca72dc1e304fa52f
Carlos Eduardo <carlos.contato00@gmail.com>
2018-08-08 11:33:36 -0300

    Update do Script

--- script.py
+++ script.py
@@ -1,7 +1,10 @@
+#coding: utf-8
 import os
 import commands
 
+drop = os.system("sudo sync && sudo sysctl vm.drop_caches=3")
 
-os.system("ls")
-
-print "Comando executado com sucesso !"
+if(drop == 0):
+	print "Comando executado com sucesso"
+else:
+	print "Nao foi possível executar este comando"
